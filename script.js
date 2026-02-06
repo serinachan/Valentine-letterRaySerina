64842945ac17903c1adb110efc1273b9ea94174e
serinachan <alifahistighfarina@gmail.com>
2026-02-06 16:08:20 +0700

    Add heart animation effect to script.js

---
+++ script.js
@@ -0,0 +1,12 @@
+setInterval(() => {
+  const heart = document.createElement("div");
+  heart.className = "heart";
+  heart.innerText = "💗";
+  heart.style.left = Math.random() * 100 + "vw";
+  heart.style.animationDuration = Math.random() * 3 + 2 + "s";
+  document.body.appendChild(heart);
+
+  setTimeout(() => {
+    heart.remove();
+  }, 5000);
+}, 300);
